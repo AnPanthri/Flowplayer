fe977f5eff7663f1abb1ec720dc4dfcb054f7a44
Anant <anant.shripanthri@gmail.com>
2017-06-21 13:19:58 -0500

    freeflowplayer

---
+++ FreeFlowPlayer/src/main/java/com/example/FreeFlowPlayer/FreeFlowPlayerApplication.java
@@ -0,0 +1,12 @@
+package com.example.FreeFlowPlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeFlowPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeFlowPlayerApplication.class, args);
+	}
+}
