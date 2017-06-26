a87726c8b1e6583956f78c5021fdd94800e1f1a6
Anant <anant.panthri@copart.com>
2017-06-26 11:44:37 -0500

    Add files via upload

---
+++ FlowPlayer/src/main/java/com/example/FreeFlowPlayer/FreeFlowPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.FreeFlowPlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeFlowPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeFlowPlayerApplication.class, args);
+	}
+}
+
